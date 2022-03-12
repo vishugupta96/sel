fe154eca77bdc0127fb45d2a7d63bde1a0164f14
your_username~ <30354305+vishugupta96@users.noreply.github.com>
2022-03-12 20:43:22 +0530

    main

--- main.py
+++ main.py
@@ -3,16 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
-import time
 
-
-import json
-import pandas as pd
-
-
-  
-
-import time  
 import os
    
 from selenium.webdriver.common.keys import Keys  
